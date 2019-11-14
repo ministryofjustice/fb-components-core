1d974d59de20aaf963a26eaed86d3549f0f115c3
Jonathan Perry <jonathanperry@sequencemedia.net>
2019-11-14 13:12:36 +0000

    Mocha

--- test/specifications/validations/validations.schema.spec.js
+++ test/specifications/validations/validations.schema.spec.js
@@ -15,7 +15,7 @@ const ajv = new Ajv({schemas})
 
 const validator = ajv.compile(jsonSchema)
 
-xdescribe('~/specifications/validations/validations.schema.json', () => {
+describe('~/specifications/validations/validations.schema.json', () => {
   describe('The data object', () => it('has properties', () => expect(dataObject).not.to.be.empty))
 
   describe('The json schema', () => it('validates', () => expect(validator(dataObject)).to.be.true))
