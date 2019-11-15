f519f4c87acb663289982e7b29eb956a8f7ec80e
Jonathan Perry <jonathanperry@sequencemedia.net>
2019-11-15 16:40:07 +0000

    Dereferencing

--- test/dereference.js
+++ test/dereference.js
@@ -8,7 +8,7 @@ module.exports = async (jsonSchema) => $RefParser.dereference(jsonSchema, {
       order: 1,
       canRead: /^http:\/\/gov.uk\/schema\/v1\.0\.0/i,
       async read (file, done) {
-      	return require(file.url.replace(/^http:\/\/gov.uk\/schema\/v1\.0\.0/, '~/specifications'))
+        return require(file.url.replace(/^http:\/\/gov.uk\/schema\/v1\.0\.0/, '~/specifications'))
       }
     }
   }
