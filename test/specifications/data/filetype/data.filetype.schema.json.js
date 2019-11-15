aa9c1553a85ec8ab11a21ce7aeb3216ad2f9e431
Jonathan Perry <jonathanperry@sequencemedia.net>
2019-11-15 17:51:44 +0000

    Typos

--- test/specifications/data/filetype/data.filetype.schema.json.js
+++ test/specifications/data/filetype/data.filetype.schema.json.js
@@ -13,7 +13,7 @@ const jsonSchema = require('~/specifications/data/filetype/data.filetype.schema.
 
 const ajv = new Ajv()
 
-describe('~/specifications/date/date.schema.json', () => {
+describe('~/specifications/data/filetype/data.filetype.schema.json', () => {
   describe('The data object', () => it('has properties', () => expect(dataObject).not.to.be.empty))
 
   describe('The json schema', () => it('validates the data object', async () => {
--- test/specifications/definition/block/definition.block.schema.spec.js
+++ test/specifications/definition/block/definition.block.schema.spec.js
@@ -13,7 +13,7 @@ const jsonSchema = require('~/specifications/definition/block/definition.block.s
 
 const ajv = new Ajv()
 
-describe('~/specifications/date/date.schema.json', () => {
+describe('~/specifications/definition/block/definition.block.schema.json', () => {
   describe('The data object', () => it('has properties', () => expect(dataObject).not.to.be.empty))
 
   describe('The json schema', () => it('validates the data object', async () => {
