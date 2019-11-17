75f4006e08e8243c19d34f644fa2a9fdcec3e9c3
Jonathan Perry <jonathanperry@sequencemedia.net>
2019-11-17 12:43:09 +0000

    Schemas

--- test/schemas.js
+++
@@ -1,8 +0,0 @@
-require('module-alias/register')
-
-const path = require('path')
-const glob = require('glob-all')
-
-module.exports = glob
-  .sync('./specifications/**/*/*.schema.json')
-  .reduce((accumulator, f) => accumulator.concat(require(path.resolve(f))), [])
