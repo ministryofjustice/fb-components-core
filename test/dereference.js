d69009d9e957a0eebaef18ec898ce4a3d4c408df
Jonathan Perry <jonathanperry@sequencemedia.net>
2019-11-17 12:19:25 +0000

    Dereferencing

--- test/dereference.js
+++ test/dereference.js
@@ -2,7 +2,7 @@ require('module-alias/register')
 
 const $RefParser = require('json-schema-ref-parser')
 
-module.exports = async (jsonSchema) => $RefParser.dereference(jsonSchema, {
+module.exports = async (jsonSchema, dereference = {circular: false}) => $RefParser.dereference(jsonSchema, {
   resolve: {
     schema: {
       order: 1,
@@ -11,5 +11,8 @@ module.exports = async (jsonSchema) => $RefParser.dereference(jsonSchema, {
         return require(file.url.replace(/^http:\/\/gov.uk\/schema\/v1\.0\.0/, '~/specifications'))
       }
     }
+  },
+  dereference: {
+    ...dereference
   }
 })
